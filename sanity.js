2624d1eb558edabf02c4600c2cd5f4ed01788b35
Bug-trackerBade <adeola45@rocketmail.com>
2022-01-22 15:41:11 +0100

    pfs E2E

---
+++ sanity.js
@@ -0,0 +1,11 @@
+import{research,graduate} from './selector'
+describe('dfrtdf',function(){
+beforeEach(function(){
+    cy.visit('/')
+})
+it('',function(){
+cy.get(research.sleep).click()
+})
+
+
+})
---
+++ selector.js
@@ -0,0 +1,9 @@
+export const research ={
+'sleep':'',
+'ahout':''
+}
+
+export const graduate={
+
+    
+}
